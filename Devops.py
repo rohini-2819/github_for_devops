f8ba7b22715c0fa665ffdf2cb6563f0c1fb8cdbd
rohini-divekar <rohini799919@gmail.com>
2024-11-16 12:28:47 +0530

    Commands

--- Devops.py
+++ Devops.py
@@ -1,1 +1,28 @@
-print("Devops Enginner")
+   7 git init
+   8 dir -a
+   9 ls -Hidden
+  10 clear
+  11 git status
+  12 python testing.py
+  13 & C:/Users/ROHINI/AppData/Local/Programs/Python/Python313/python.exe c:/Users/ROHINI/Desktop/GITHUB-FOR-DEVOPS/testing.py
+  14 git status
+  15 git add testing.py
+  16 git status
+  17 git rm --cached testing.py
+  18 git status
+  19 git add .
+  20 git status
+  21 clear
+  22 git commit -m "added testing files"
+  23 git status
+  24 rm testing.py
+  25 ls
+  26 git status
+  27 git restore testing.py
+  28 ls
+  29 git status
+  30 & C:/Users/ROHINI/AppData/Local/Programs/Python/Python313/python.exe c:/Users/ROHINI/Desktop/GITHUB-FOR-DEVOPS/Git_commands_txt.py
+  31 clear4
+  32 clear
+ git status
+ git add .
